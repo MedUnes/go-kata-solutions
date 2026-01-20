08a67edb6d998ba1ff2ac00716b648849dff2dd7
medunes <medunes@protonmail.com>
2026-01-20 18:30:10 +0100

    fix first test

--- 01-context-cancellation-concurrency/01-concurrent-aggregator/aggregator_test.go
+++ 01-context-cancellation-concurrency/01-concurrent-aggregator/aggregator_test.go
@@ -3,6 +3,7 @@ package concurrent_aggregator
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"testing"
 	"time"
 
@@ -66,48 +67,79 @@ func (os OrderServiceMock) GetAll(ctx context.Context, userId int) ([]*order.Ord
 
 }
 func TestAggregate(t *testing.T) {
-	t.Run("Test aggregator function", func(t *testing.T) {
-		ctx := context.Background()
-		orderService := &OrderServiceMock{
-			simulatedDuration: 2 * time.Second,
-			simulatedOrders: []*order.Order{
-				{Id: 1, UserId: 1, Cost: 100.0},
-				{Id: 2, UserId: 1, Cost: 20.6},
-				{Id: 3, UserId: 3, Cost: 30.79},
+	type Input struct {
+		profileService    ProfileServiceMock
+		orderService      OrderServiceMock
+		searchedProfileId int
+	}
+	type Expected struct {
+		aggregatedProfile *AggregatedProfile
+		err               error
+	}
+	type TestCase struct {
+		name     string
+		input    Input
+		expected Expected
+	}
+	testCases := []TestCase{
+		{
+			"successful use case 1",
+			Input{
+				ProfileServiceMock{
+					1 * time.Second,
+					nil,
+					[]*profile.Profile{
+						{Id: 1, Name: "Alice"},
+						{Id: 2, Name: "Bob"},
+						{Id: 3, Name: "Charlie"},
+						{Id: 4, Name: "Dave"},
+						{Id: 5, Name: "Eva"},
+					},
+				},
+				OrderServiceMock{
+					2 * time.Second,
+					nil,
+					[]*order.Order{
+						{Id: 1, UserId: 1, Cost: 100.0},
+						{Id: 2, UserId: 1, Cost: 20.6},
+						{Id: 3, UserId: 3, Cost: 30.79},
+					},
+				},
+				1,
 			},
-			simulatedError: nil,
-		}
-		profileService := &ProfileServiceMock{
-			simulatedDuration: 1 * time.Second,
-			simulatedProfiles: []*profile.Profile{
-				{Id: 1, Name: "Alice"},
-				{Id: 2, Name: "Bob"},
-				{Id: 3, Name: "Charlie"},
-				{Id: 4, Name: "Dave"},
-				{Id: 5, Name: "Eva"},
+			Expected{
+				aggregatedProfile: &AggregatedProfile{
+					Name: "Alice",
+					Cost: 100.0,
+				},
 			},
-			simulatedError: nil,
-		}
-		u := NewUserAggregator(
-			orderService,
-			profileService,
-			func(ua *UserAggregator) {
-				ua.timeout = 3 * time.Second
-			},
-			func(ua *UserAggregator) {
-				ua.timeout = 3 * time.Second
-			},
-		)
-		aggregatedProfiles, err := u.Aggregate(ctx, 1)
+		},
+	}
+	for _, testCase := range testCases {
 
-		assert.Nil(t, err)
-		assert.GreaterOrEqual(t, len(aggregatedProfiles), 1)
+		t.Run("Test aggregator function", func(t *testing.T) {
+			ctx := context.Background()
+			u := NewUserAggregator(
+				testCase.input.orderService,
+				testCase.input.profileService,
+				func(ua *UserAggregator) {
+					ua.timeout = 3 * time.Second
+				},
+				func(ua *UserAggregator) {
+					ua.logger = slog.Default()
+				},
+			)
+			aggregatedProfiles, err := u.Aggregate(ctx, 1)
 
-		aggregatedProfile := aggregatedProfiles[0]
-		assert.NotNil(t, aggregatedProfile)
+			assert.Nil(t, err)
+			assert.GreaterOrEqual(t, len(aggregatedProfiles), 1)
 
-		assert.NotNil(t, aggregatedProfile)
-		assert.Equal(t, "Alice", aggregatedProfile.Name)
-		assert.Equal(t, 100.0, aggregatedProfile.Cost)
-	})
+			aggregatedProfile := aggregatedProfiles[0]
+			assert.NotNil(t, aggregatedProfile)
+
+			assert.NotNil(t, aggregatedProfile)
+			assert.Equal(t, "Alice", aggregatedProfile.Name)
+			assert.Equal(t, 100.0, aggregatedProfile.Cost)
+		})
+	}
 }
